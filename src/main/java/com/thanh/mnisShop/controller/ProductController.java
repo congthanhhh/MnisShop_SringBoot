fd8d64c4af3df3e44bd14e7cf0d99ef222852fa7
congthanhhh <luffymonkeydct@gmail.com>
2023-08-28 00:35:32 +0700

    hien thi product cho home, shop, detail

--- src/main/java/com/thanh/mnisShop/controller/ProductController.java
+++ src/main/java/com/thanh/mnisShop/controller/ProductController.java
@@ -1,27 +1,39 @@
 package com.thanh.mnisShop.controller;
 
+import com.thanh.mnisShop.model.Product;
+import com.thanh.mnisShop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    ProductService productService;
 
     @RequestMapping("/home")
-    public String home() {
-
+    public String home(Model model) {
+        List<Product> items = productService.findAll();
+        model.addAttribute("items", items);
         return "home/product/homeProduct";
     }
 
-    @RequestMapping("/detail")
-    public String detail() {
-
+    @RequestMapping("/detail/{id}")
+    public String detail(@PathVariable("id") Integer id, Model model) {
+        Product items = productService.findById(id);
+        model.addAttribute("detailPr", items);
         return "home/product/detailProduct";
     }
 
     @RequestMapping("/shop")
-    public String shop() {
-
+    public String shop(Model model) {
+        List<Product> items = productService.findAll();
+        model.addAttribute("items", items);
         return "home/product/shopProduct";
     }
 
---
+++ src/main/java/com/thanh/mnisShop/interceptor/CategoriesInterceptor.java
@@ -0,0 +1,20 @@
+package com.thanh.mnisShop.interceptor;
+
+import com.thanh.mnisShop.service.CategoryService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class CategoriesInterceptor implements HandlerInterceptor {
+    @Autowired
+    CategoryService categoryService;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        request.setAttribute("categories", categoryService.findAll());
+    }
+}
